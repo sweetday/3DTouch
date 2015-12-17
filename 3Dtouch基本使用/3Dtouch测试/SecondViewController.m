8ca29d721efb24bf9cc7f2f59affb204e828e8fb
zyz <mrzhang99yx@163.com>
2015-12-17 16:54:07 +0800

    modify

--- 3Dtouch基本使用/3Dtouch测试/SecondViewController.m
+++ 3Dtouch基本使用/3Dtouch测试/SecondViewController.m
@@ -9,7 +9,6 @@
 #import "SecondViewController.h"
 
 @interface SecondViewController ()
-@property (nonatomic, strong) NSArray *previewActions;
 @end
 
 @implementation SecondViewController
@@ -22,17 +21,20 @@
     self.view.backgroundColor = [UIColor greenColor];
     
     // 创建预览下面的按钮
-    self.previewActions = @[
-                            [UIPreviewAction actionWithTitle:@"请点击我" style:UIPreviewActionStyleDefault handler:^(UIPreviewAction * _Nonnull action, UIViewController * _Nonnull previewViewController) {
-                                NSLog(@"你点到我了");
-                            }]];
+//    self.previewActions = @[
+//                            [UIPreviewAction actionWithTitle:@"请点击我" style:UIPreviewActionStyleDefault handler:^(UIPreviewAction * _Nonnull action, UIViewController * _Nonnull previewViewController) {
+//                                NSLog(@"你点到我了");
+//                            }]];
 
 }
 
 // 预览下面的按钮
 - (NSArray<id<UIPreviewActionItem>> *)previewActionItems
 {
-    return self.previewActions;
+    return @[
+             [UIPreviewAction actionWithTitle:@"请点击我" style:UIPreviewActionStyleDefault handler:^(UIPreviewAction * _Nonnull action, UIViewController * _Nonnull previewViewController) {
+                 NSLog(@"你点到我了");
+             }]];
 }
 
 
